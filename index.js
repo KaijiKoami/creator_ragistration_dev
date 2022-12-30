9556d7bbb47f0b21b25b1e93208221ca247776b0
KaijiKoami <kaiji.koami@solezore.co.jp>
2022-12-30 16:08:20 +0000

    dev

---
+++ index.js
@@ -0,0 +1,21 @@
+import './style.css';
+import liff from '@line/liff'
+
+document.addEventListener("DOMContentLoaded", function () {
+    liff
+        .init({ liffId: process.env.LIFF_ID })
+        .then(() => {
+            console.log("Success! you can do something with LIFF API here.")
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    liff
+        .getProfile()
+        .then((profile) => {
+            const name = profile.displayName;
+        })
+        .catch((err) => {
+            console.log("error", err);
+        });
+});
