c85d89b4cfdee8a6d6d7ecb9eaa52839a4d696f3
KaijiKoami <kaiji.koami@solezore.co.jp>
2022-12-31 03:30:14 +0000

    test

--- index.js
+++ index.js
@@ -3,19 +3,23 @@ import liff from '@line/liff'
 
 document.addEventListener("DOMContentLoaded", function () {
     liff
-        .init({ liffId: process.env.LIFF_ID })
+        .init({ 
+            liffId: "1657773262-O5ENDkKN",
+            withLoginOnExternalBrowser: true,
+        })
         .then(() => {
             console.log("Success! you can do something with LIFF API here.")
         })
         .catch((error) => {
             console.log(error)
         })
-    liff
-        .getProfile()
-        .then((profile) => {
-            const name = profile.displayName;
-        })
-        .catch((err) => {
-            console.log("error", err);
-        });
+
+    // liff
+    //     .getProfile()
+    //     .then((profile) => {
+    //         const name = profile.displayName;
+    //     })
+    //     .catch((err) => {
+    //         console.log("error", err);
+    //     });
 });
